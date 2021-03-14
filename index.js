ca29ded16a70c05fd0f59b356afcaf5412611d03
Elijah Schiltz-Rouse <elijahforest101@gmail.com>
2021-03-14 19:55:23 -0400

    Completed assignement

---
+++ index.js
@@ -0,0 +1,8 @@
+function introduction(name) {
+   console.log('Hi, my name is ${name});
+}
+
+function introductionWithLanguage(name, language= JavaScript) {
+   console.log('Hi, my name is ${name} and I am learning to program in ${language});
+}
+
