8c4206b192a6434d2754a599db1b1fc493871fe3
Elijah Schiltz-Rouse <elijahforest101@gmail.com>
2021-03-31 12:10:20 -0400

    Completed Assignmnet

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-function introduction(name) {
-   console.log('Hi, my name is ${name});
+
+introduction();
+introductionWithLanguage();
+
+function introduction(name){
+   //console.log(`Hi, my name is ${name}.`);
+   return `Hi, my name is ${name}.`;
+  
 }
 
-function introductionWithLanguage(name, language= "JavaScript") {
-   console.log('Hi, my name is ${name} and I am learning to program in ${language});
+
+function introductionWithLanguage(name, language = "JavaScript"){
+   //console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+   return `Hi, my name is ${name} and I am learning to program in ${language}.`;
 }
+
+function introductionWithLanguageOptional(name, language = "JavaScript"){
+   //console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+   return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+
 
